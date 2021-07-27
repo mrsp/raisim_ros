eaa45b240a49b02eb74cc4e3a879d287bad54e6e
mrsp <stpiperakis@gmail.com>
2021-07-27 17:36:09 +0300

    added a buffer the command joint state msgs

--- src/nao_ros.cpp
+++ src/nao_ros.cpp
@@ -22,38 +22,21 @@
 #include "nav_msgs/Odometry.h"
 #include "geometry_msgs/WrenchStamped.h"
 #include "geometry_msgs/PointStamped.h"
-
+#include "raisim_ros/Queue.h"
 
 using namespace std::chrono;
 using namespace Eigen;
 //Simulation Step, 0.01 is the hardware loop of the actual NAO
 double dt = 0.01;
-int micro_dt = 10000;
+double freq = 1.0 / dt;
+int micro_dt = dt * 1000000;
+Queue<sensor_msgs::JointStateConstPtr> joint_data;
 
-Eigen::VectorXd jointNominalConfig, jointVelocityTarget;
-
-void commandJointStatesCallback(const sensor_msgs::JointState::ConstPtr& msg)
+void commandJointStatesCallback(const sensor_msgs::JointState::ConstPtr &msg)
 {
-
-  if(msg->position.size() > 0)
-  {
-    std::vector<double> pos_vector = msg->position;
-    double* pos_array = pos_vector.data();
-    jointNominalConfig.tail(jointNominalConfig.size() - 7) = Eigen::Map<Eigen::Matrix<double, 26, 1> >(pos_array);
-
-    if(msg->velocity.size()>0)
-    {
-      std::vector<double> vel_vector = msg->velocity;
-      double* vel_array = vel_vector.data();
-      jointVelocityTarget.tail(jointVelocityTarget.size() - 6) = Eigen::Map<Eigen::Matrix<double, 26, 1> >(vel_array);
-    }
-    else
-    {
-      jointVelocityTarget.setZero();
-    }
-  }
-
-
+  joint_data.push(msg);
+  if (joint_data.size() > (int)freq / 10)
+    joint_data.pop();
 }
 
 int main(int argc, char *argv[])
@@ -68,7 +51,7 @@ int main(int argc, char *argv[])
   raisim::World world;
   world.setTimeStep(dt);
   world.setERP(0, 0);
-  // world.setGravity(Eigen::Vector3d(0,0,0));
+  //world.setGravity(Eigen::Vector3d(0,0,0));
 
   /// create objects
   auto ground = world.addGround();
@@ -84,32 +67,32 @@ int main(int argc, char *argv[])
   if (itr_ != jnames.end())
     jnames.erase(itr_);
 
-  cout<<"Joint Names "<<endl;
-  for (int i=0; i<jnames.size();i++)
-    cout <<jnames[i]<<endl;;
+  cout << "Joint Names " << endl;
+  for (int i = 0; i < jnames.size(); i++)
+    cout << jnames[i] << endl;
+  ;
 
+  Eigen::VectorXd jointNominalConfig, jointNominalVelocity;
 
   ///Set Nominal Configuration
   jointNominalConfig.resize(NAO->getGeneralizedCoordinateDim());
-  jointVelocityTarget.resize(NAO->getDOF());
+  jointNominalVelocity.resize(NAO->getDOF());
   jointNominalConfig.setZero();
-  jointNominalConfig << 0, 0, 0.32  , 1, 0, 0, 0,
+  jointNominalConfig << 0, 0, 0.32, 1, 0, 0, 0,
       0.0, 0.0,
       0.0, 0.0, -0.3976, 0.85, -0.4427, -0.009,
       0.0, 0.0, -0.3976, 0.85, -0.4427, -0.009,
       1.5, 0.15, 0, -0.0349066, -1.5, 0,
-      1.5, -0.15, 0, 0.0349066,  1.5, 0;
-
-  jointVelocityTarget.setZero();
+      1.5, -0.15, 0, 0.0349066, 1.5, 0;
 
   ///Set Joint PD Gains
   Eigen::VectorXd jointPgain(NAO->getDOF()), jointDgain(NAO->getDOF());
-  jointPgain.setConstant(500);
+  jointPgain.setConstant(350);
   jointDgain.setConstant(5); //Gazebo D gain is 0.1 but makes NAO unstable in raisim
 
   ///Set the Initial Configuration in the world
   NAO->setGeneralizedCoordinate(jointNominalConfig);
-  NAO->setGeneralizedVelocity(jointVelocityTarget);
+  NAO->setGeneralizedVelocity(jointNominalVelocity);
   NAO->setGeneralizedForce(Eigen::VectorXd::Zero(NAO->getDOF()));
   NAO->setPdGains(jointPgain, jointDgain);
   NAO->setName("NAO");
@@ -119,14 +102,8 @@ int main(int argc, char *argv[])
   server.launchServer();
   server.focusOn(NAO);
 
-  /// q, dq is the actual configuration from raisim, qd, dqd the desired configuration computed with WBC
+  /// q, dq is the actual configuration from raisim
   Eigen::VectorXd q(NAO->getGeneralizedCoordinateDim()), dq(NAO->getDOF());
-  Eigen::VectorXd qd(NAO->getGeneralizedCoordinateDim()), dqd(NAO->getDOF());
-  qd = jointNominalConfig;
-  dqd = jointVelocityTarget;
-
-  /// swap to XYZW since wbc assumes that order but raisim assumes WXYZ
-  swapQuatXYZW(jointNominalConfig);
 
   /// Set Desired Frames for the WBC
   string lfoot_frame = "l_sole";
@@ -136,11 +113,9 @@ int main(int argc, char *argv[])
   string rhand_frame = "RHand";
   string head_frame = "Head";
 
-
-
   Vector3d CoM_pos_ref, CoM_vel_ref, CoM_acc_ref, CoM_vel_ref_, lhand_pos_ref, rhand_pos_ref, lhand_linear_vel_ref, rhand_linear_vel_ref, head_pos_ref, head_linear_vel_ref,
-  lf_pos_ref, rf_pos_ref, lf_linear_vel_ref, rf_linear_vel_ref, lf_angular_vel_ref, rf_angular_vel_ref, base_pos_ref, base_linear_vel_ref, base_angular_vel_ref,
-  lhand_angular_vel_ref, rhand_angular_vel_ref, head_angular_vel_ref;
+      lf_pos_ref, rf_pos_ref, lf_linear_vel_ref, rf_linear_vel_ref, lf_angular_vel_ref, rf_angular_vel_ref, base_pos_ref, base_linear_vel_ref, base_angular_vel_ref,
+      lhand_angular_vel_ref, rhand_angular_vel_ref, head_angular_vel_ref;
   Vector3d CoM_pos, CoM_vel, CoM_vel_, CoM_acc;
   Quaterniond lf_orientation_ref, rf_orientation_ref, base_orientation_ref, head_orientation_ref, lhand_orientation_ref, rhand_orientation_ref;
   VectorXd joint_states_ref;
@@ -151,7 +126,6 @@ int main(int argc, char *argv[])
   int init_idx = 0;
   bool LSS, RSS, DS;
   bool firstCoMVel = true;
-
 
   /// For Contact Detection, COP, GRF and GRT computation
   auto RfootIndex = NAO->getBodyIdx("r_ankle");
@@ -169,14 +143,11 @@ int main(int argc, char *argv[])
   Quaterniond qir, qil;
   Vector3d COPL, COPR, ZMP;
 
-
   double dist_t = 0;
   int idx = 0;
   int start_idx = 500;
 
-
-  // std::vector<double> q_vec(q_size,0), dq_vec(dq_size,0);
-// INIT ROS PUBLISHER
+  // INIT ROS PUBLISHER
 
   ros::init(argc, argv, "nao_ros_publisher");
   ros::NodeHandle n;
@@ -191,138 +162,146 @@ int main(int argc, char *argv[])
   ros::Publisher odom_pub_RLeg = n.advertise<nav_msgs::Odometry>("/nao_raisim_ros/RLeg/odom", 1000);
   ros::Publisher zmp_pub = n.advertise<geometry_msgs::PointStamped>("/nao_raisim_ros/ZMP", 1000);
   ros::Subscriber command_js_sub = n.subscribe("/nao_raisim_ros/command_joint_states", 1000, commandJointStatesCallback);
-  ros::Rate loop_rate(100);
+  ros::Rate loop_rate(120);
 
   while (ros::ok())
   {
 
     auto start = high_resolution_clock::now();
 
-      NAO->getState(q, dq);
-      swapQuatXYZW(q);
+    NAO->getState(q, dq);
 
-      /// Get Leg Positions in raisim
-      NAO->getFramePosition(RfootFrameIndex, footPosition);
-      NAO->getFrameOrientation(RfootFrameIndex, footOrientation);
-      RfootPosition = Eigen::Vector3d(footPosition[0], footPosition[1], footPosition[2]);
-      RfootOrientation << footOrientation[0], footOrientation[1], footOrientation[2], footOrientation[3], footOrientation[4], footOrientation[5], footOrientation[6], footOrientation[7], footOrientation[8];
-      //std::cout<<"Right Foot Pos"<< RfootPosition.transpose()<<std::endl;
+    /// Get Leg Positions in raisim
+    NAO->getFramePosition(RfootFrameIndex, footPosition);
+    NAO->getFrameOrientation(RfootFrameIndex, footOrientation);
+    RfootPosition = Eigen::Vector3d(footPosition[0], footPosition[1], footPosition[2]);
+    RfootOrientation << footOrientation[0], footOrientation[1], footOrientation[2], footOrientation[3], footOrientation[4], footOrientation[5], footOrientation[6], footOrientation[7], footOrientation[8];
+    //std::cout<<"Right Foot Pos"<< RfootPosition.transpose()<<std::endl;
 
-      Tir.translation() = RfootPosition;
-      Tir.linear() = RfootOrientation;
-      qir = Quaterniond(RfootOrientation);
+    Tir.translation() = RfootPosition;
+    Tir.linear() = RfootOrientation;
+    qir = Quaterniond(RfootOrientation);
 
-      NAO->getFramePosition(LfootFrameIndex, footPosition);
-      NAO->getFrameOrientation(LfootFrameIndex, footOrientation);
-      LfootPosition = Eigen::Vector3d(footPosition[0], footPosition[1], footPosition[2]);
-      //std::cout<<"Left Foot Pos"<< LfootPosition.transpose()<<std::endl;
+    NAO->getFramePosition(LfootFrameIndex, footPosition);
+    NAO->getFrameOrientation(LfootFrameIndex, footOrientation);
+    LfootPosition = Eigen::Vector3d(footPosition[0], footPosition[1], footPosition[2]);
+    //std::cout<<"Left Foot Pos"<< LfootPosition.transpose()<<std::endl;
 
-      LfootOrientation << footOrientation[0], footOrientation[1], footOrientation[2], footOrientation[3], footOrientation[4], footOrientation[5], footOrientation[6], footOrientation[7], footOrientation[8];
-      Til.translation() = LfootPosition;
-      Til.linear() = LfootOrientation;
-      qil = Quaterniond(LfootOrientation);
+    LfootOrientation << footOrientation[0], footOrientation[1], footOrientation[2], footOrientation[3], footOrientation[4], footOrientation[5], footOrientation[6], footOrientation[7], footOrientation[8];
+    Til.translation() = LfootPosition;
+    Til.linear() = LfootOrientation;
+    qil = Quaterniond(LfootOrientation);
 
-      //std::cout<<"Left Foot Pos"<< LfootOrientation<<std::endl;
+    //std::cout<<"Left Foot Pos"<< LfootOrientation<<std::endl;
 
-      RLegGRT.setZero();
-      LLegGRT.setZero();
-      RLegGRF.setZero();
-      LLegGRF.setZero();
-      RSS = false;
-      LSS = false;
-      DS = false;
+    RLegGRT.setZero();
+    LLegGRT.setZero();
+    RLegGRF.setZero();
+    LLegGRF.setZero();
+    RSS = false;
+    LSS = false;
+    DS = false;
+    COPL.setZero();
+    COPR.setZero();
+    /// for all contacts on the robot, check ...
+    for (auto &contact : NAO->getContacts())
+    {
+      if (contact.skip())
+        continue; /// if the contact is internal, one contact point is set to 'skip'
+      if (RfootIndex == contact.getlocalBodyIndex() && fabs(contact.getPosition().e()(2) - RfootPosition(2)) < 0.015)
+      {
+        footForce = contact.getContactFrame().e().transpose() * contact.getImpulse().e() / dt;
+        RLegGRF += footForce;
+        COPR += contact.getPosition().e() * footForce(2);
+        footTorque = (contact.getPosition().e() - RfootPosition).cross(footForce);
+        RLegGRT += footTorque;
+        //cout<<"Right Contact Point "<<(contact.getPosition().e() ).transpose()<<" "<<contact.isObjectA()<<endl;
+        if (contact.isObjectA())
+          RSS = true;
+      }
+      if (LfootIndex == contact.getlocalBodyIndex() && fabs(contact.getPosition().e()(2) - LfootPosition(2)) < 0.015)
+      {
+        footForce = contact.getContactFrame().e().transpose() * contact.getImpulse().e() / dt;
+        LLegGRF += footForce;
+        //cout<<"Left Contact Point "<<(contact.getPosition().e()).transpose()<<" "<<contact.isObjectA()<<endl;
+        COPL += contact.getPosition().e() * footForce(2);
+        footTorque = (contact.getPosition().e() - LfootPosition).cross(footForce);
+        LLegGRT += footTorque;
+        if (contact.isObjectA())
+          LSS = true;
+      }
+    }
+
+    if (LSS && RSS)
+      DS = true;
+
+    if (LLegGRF(2) > 7)
+    {
+      LLeg_COP = Eigen::Vector3d(-LLegGRT(1) / LLegGRF(2), LLegGRT(0) / LLegGRF(2), 0) + LfootPosition;
+      COPL = COPL / LLegGRF(2);
+      COPL(2) = LfootPosition(2);
+    }
+    else
+    {
+      LLeg_COP.setZero();
       COPL.setZero();
+    }
+    if (RLegGRF(2) > 7)
+    {
+      RLeg_COP = Eigen::Vector3d(-RLegGRT(1) / RLegGRF(2), RLegGRT(0) / RLegGRF(2), 0) + RfootPosition;
+      COPR = COPR / RLegGRF(2);
+      COPR(2) = RfootPosition(2);
+    }
+    else
+    {
+      RLeg_COP.setZero();
       COPR.setZero();
-      /// for all contacts on the robot, check ...
-      for (auto &contact : NAO->getContacts())
-      {
-        if (contact.skip())
-          continue; /// if the contact is internal, one contact point is set to 'skip'
-        if (RfootIndex == contact.getlocalBodyIndex() && fabs(contact.getPosition().e()(2) - RfootPosition(2))<0.015)
-        {
-          footForce = contact.getContactFrame().e().transpose() * contact.getImpulse()->e() / dt;
-          RLegGRF += footForce;
-          COPR += contact.getPosition().e() * footForce(2);
-          footTorque = (contact.getPosition().e() - RfootPosition).cross(footForce);
-          RLegGRT += footTorque;
-          //cout<<"Right Contact Point "<<(contact.getPosition().e() ).transpose()<<" "<<contact.isObjectA()<<endl;
-          if (contact.isObjectA())
-            RSS = true;
+    }
 
-        }
-        if (LfootIndex == contact.getlocalBodyIndex() && fabs(contact.getPosition().e()(2) - LfootPosition(2))<0.015)
-        {
-          footForce = contact.getContactFrame().e().transpose() * contact.getImpulse()->e() / dt;
-          LLegGRF += footForce;
-          //cout<<"Left Contact Point "<<(contact.getPosition().e()).transpose()<<" "<<contact.isObjectA()<<endl;
-          COPL += contact.getPosition().e() * footForce(2);
-          footTorque = (contact.getPosition().e() - LfootPosition).cross(footForce);
-          LLegGRT += footTorque;
-          if (contact.isObjectA())
-            LSS = true;
-        }
-      }
+    // cout<<"COP L "<<COPL.transpose()<<" "<<LLeg_COP.transpose()<<endl;
+    // cout<<"COP R "<<COPR.transpose()<<" "<<RLeg_COP.transpose()<<endl;
+    raisim::Vec<3> linear_momentum = NAO->getLinearMomentum();
+    raisim::Vec<3> center_of_mass = NAO->getCOM();
 
-      if (LSS && RSS)
-        DS = true;
+    ZMP = COPR + COPL;
+    ZMP(2) /= 2.0;
+    CoM_vel = Vector3d(linear_momentum(0), linear_momentum(1), linear_momentum(2)) / mass;
+    CoM_pos = Vector3d(center_of_mass(0), center_of_mass(1), center_of_mass(2));
 
+    cout << "Buffer Size " << joint_data.size() << endl;
 
-      if (LLegGRF(2) > 7)
-      {
-        LLeg_COP = Eigen::Vector3d(-LLegGRT(1) / LLegGRF(2), LLegGRT(0) / LLegGRF(2), 0) + LfootPosition;
-        COPL = COPL / LLegGRF(2);
-        COPL(2) = LfootPosition(2);
-      }
-      else
-      {
-        LLeg_COP.setZero();
-        COPL.setZero();
-      }
-      if (RLegGRF(2) > 7)
-      {
-        RLeg_COP = Eigen::Vector3d(-RLegGRT(1) / RLegGRF(2), RLegGRT(0) / RLegGRF(2), 0) + RfootPosition;
-        COPR = COPR / RLegGRF(2);
-        COPR(2) = RfootPosition(2);
-      }
-      else
-      {
-        RLeg_COP.setZero();
-        COPR.setZero();
-      }
+    if (joint_data.size() > 0)
+    {
+      sensor_msgs::JointStateConstPtr msg = joint_data.pop();
+      std::vector<double> pos_vector = msg->position;
+      double *pos_array = pos_vector.data();
+      jointNominalConfig = Eigen::Map<Eigen::Matrix<double, 33, 1>>(pos_array);
 
-      // cout<<"COP L "<<COPL.transpose()<<" "<<LLeg_COP.transpose()<<endl;
-      // cout<<"COP R "<<COPR.transpose()<<" "<<RLeg_COP.transpose()<<endl;
-      raisim::Vec<3> linear_momentum = NAO->getLinearMomentum();
-      raisim::Vec<3> center_of_mass = NAO->getCompositeCOM()[0];
+      std::vector<double> vel_vector = msg->velocity;
+      double *vel_array = vel_vector.data();
+      jointNominalVelocity = Eigen::Map<Eigen::Matrix<double, 32, 1>>(vel_array);
+    }
+    NAO->setPdTarget(jointNominalConfig, jointNominalVelocity);
 
-      ZMP = COPR + COPL;
-      ZMP(2) /= 2.0;
-      CoM_vel = Vector3d(linear_momentum(0), linear_momentum(1), linear_momentum(2)) / mass;
-      CoM_pos = Vector3d(center_of_mass(0), center_of_mass(1), center_of_mass(2));
+    //NAO->setGeneralizedCoordinate(jointNominalConfig);
+    //NAO->setGeneralizedVelocity(jointNominalVelocity);
 
-      // cout<<"Center Of Mass "<<CoM_pos.transpose()<<endl;
-
-
-
-    NAO->setPdTarget(jointNominalConfig, jointVelocityTarget);
-
-
-///////////////////////////////// PUBLISHING ///////////////////
+    ///////////////////////////////// PUBLISHING ///////////////////
 
     // JOINT PUBLISHER
     sensor_msgs::JointState joint_msg;
 
     // from Eigen:vectorXd to pub msg
     std::vector<double> qfoo(q.data() + 7, q.data() + q.size()); // convert  e to std vector
-    joint_msg.position.resize(qfoo.size()); // TODO fix small size
-    joint_msg.position = qfoo ;
+    joint_msg.position.resize(qfoo.size());                      // TODO fix small size
+    joint_msg.position = qfoo;
 
     std::vector<double> dqfoo(dq.data() + 6, dq.data() + dq.size()); // convert  e to std vector
-    joint_msg.velocity.resize(dqfoo.size()); // TODO fix small size
-    joint_msg.velocity= dqfoo ;
+    joint_msg.velocity.resize(dqfoo.size());                         // TODO fix small size
+    joint_msg.velocity = dqfoo;
 
-    joint_msg.name.resize(jnames.size()) ;
-    joint_msg.name = jnames ;
+    joint_msg.name.resize(jnames.size());
+    joint_msg.name = jnames;
 
     joint_msg.header.stamp = ros::Time::now();
 
@@ -334,17 +313,17 @@ int main(int argc, char *argv[])
     odom_msg.pose.pose.position.x = q(0);
     odom_msg.pose.pose.position.y = q(1);
     odom_msg.pose.pose.position.z = q(2);
-    odom_msg.pose.pose.orientation.x = q(3);
-    odom_msg.pose.pose.orientation.y = q(4);
-    odom_msg.pose.pose.orientation.z = q(5);
-    odom_msg.pose.pose.orientation.w = q(6);
+    odom_msg.pose.pose.orientation.x = q(4);
+    odom_msg.pose.pose.orientation.y = q(5);
+    odom_msg.pose.pose.orientation.z = q(6);
+    odom_msg.pose.pose.orientation.w = q(3);
 
-    odom_msg.twist.twist.linear.x = dq(0) ;
-    odom_msg.twist.twist.linear.y = dq(1) ;
-    odom_msg.twist.twist.linear.z = dq(2) ;
-    odom_msg.twist.twist.angular.x = dq(3) ;
-    odom_msg.twist.twist.angular.y = dq(4) ;
-    odom_msg.twist.twist.angular.z = dq(5) ;
+    odom_msg.twist.twist.linear.x = dq(0);
+    odom_msg.twist.twist.linear.y = dq(1);
+    odom_msg.twist.twist.linear.z = dq(2);
+    odom_msg.twist.twist.angular.x = dq(3);
+    odom_msg.twist.twist.angular.y = dq(4);
+    odom_msg.twist.twist.angular.z = dq(5);
 
     odom_msg.header.frame_id = "world";
     odom_msg.child_frame_id = "base_link";
@@ -364,8 +343,6 @@ int main(int argc, char *argv[])
     rodom_msg.pose.pose.orientation.z = qir.z();
     rodom_msg.pose.pose.orientation.w = qir.w();
 
-
-
     rodom_msg.header.frame_id = "world";
     rodom_msg.child_frame_id = "r_sole";
 
@@ -384,8 +361,6 @@ int main(int argc, char *argv[])
     lodom_msg.pose.pose.orientation.z = qil.z();
     lodom_msg.pose.pose.orientation.w = qil.w();
 
-
-
     lodom_msg.header.frame_id = "world";
     lodom_msg.child_frame_id = "l_sole";
 
@@ -393,13 +368,9 @@ int main(int argc, char *argv[])
 
     odom_pub_LLeg.publish(lodom_msg);
 
-
-
-
-
     // FORCE TORQUE PUBLISHER
 
-    geometry_msgs::WrenchStamped wrench_msg ;
+    geometry_msgs::WrenchStamped wrench_msg;
 
     wrench_msg.wrench.force.x = LLegGRF(0);
     wrench_msg.wrench.force.y = LLegGRF(1);
@@ -414,7 +385,6 @@ int main(int argc, char *argv[])
     wrench_msg.header.stamp = ros::Time::now();
     wrench_pub_LLeg.publish(wrench_msg);
 
-
     wrench_msg.wrench.force.x = RLegGRF(0);
     wrench_msg.wrench.force.y = RLegGRF(1);
     wrench_msg.wrench.force.z = RLegGRF(2);
@@ -427,7 +397,6 @@ int main(int argc, char *argv[])
 
     wrench_msg.header.stamp = ros::Time::now();
     wrench_pub_RLeg.publish(wrench_msg);
-
 
     // GAIT PHASE PUBLISHER
 
@@ -451,10 +420,9 @@ int main(int argc, char *argv[])
     com_msg.pose.pose.position.y = CoM_pos(1);
     com_msg.pose.pose.position.z = CoM_pos(2);
 
-
-    com_msg.twist.twist.linear.x = CoM_vel(0) ;
-    com_msg.twist.twist.linear.y = CoM_vel(1) ;
-    com_msg.twist.twist.linear.z = CoM_vel(2) ;
+    com_msg.twist.twist.linear.x = CoM_vel(0);
+    com_msg.twist.twist.linear.y = CoM_vel(1);
+    com_msg.twist.twist.linear.z = CoM_vel(2);
 
     com_msg.header.frame_id = "world";
     com_msg.child_frame_id = "com";
@@ -476,8 +444,7 @@ int main(int argc, char *argv[])
 
     zmp_pub.publish(zmp_msg);
 
-
-////////////////////////////////// INTERGRATE ///////////////////////
+    ////////////////////////////////// INTERGRATE ///////////////////////
 
     server.integrateWorldThreadSafe();
 
@@ -488,12 +455,9 @@ int main(int argc, char *argv[])
     // if(loop_duration>0)
     //   std::this_thread::sleep_for(std::chrono::microseconds(loop_duration));
 
+    ros::spinOnce();
 
-      ros::spinOnce();
-
-      loop_rate.sleep();
-
-
+    loop_rate.sleep();
   }
 
   server.killServer();
